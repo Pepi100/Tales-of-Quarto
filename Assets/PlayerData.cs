35702536aa7f2c7203c452de0009f7457139edcf
EricMorosan <morosan.eric2002@gmail.com>
2023-11-20 02:39:15 +0200

    First animations and achievements

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData : MonoBehaviour
+{
+    #region Singleton
+    public static PlayerData instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+
+    public float x = 0, y = 0, z = 0;
+
+    private bool[] _achievementsIds = new bool[] { false, true, false, false, false, false, false };
+
+
+
+    ///Call this method with the id of the achievement to mark as checked
+
+
+
+    public void Add(int idAchiv)
+    {
+        if (!_achievementsIds[idAchiv])
+            _achievementsIds[idAchiv] = true;
+    }
+
+    public void Remove(int idAchiv)
+    {
+        _achievementsIds[idAchiv] = false;
+    }
+
+
+    ///Getter method
+    public bool[] getAchievements()
+    {
+        return _achievementsIds;
+    }
+
+
+
+    public float getX()
+    {
+        return x;
+    }
+
+    public float getY()
+    {
+        return y;
+    }
+
+    public float getZ()
+    {
+        return z;
+    }
+
+
+
+
+    public void setAchievements(bool[] new_achievementsIds)
+    {
+        _achievementsIds = new_achievementsIds;
+    }
+
+    public void setX(double newX)
+    {
+        x = (float)newX;
+    }
+
+    public void setY(double newY)
+    {
+        y = (float)newY;
+    }
+
+    public void setZ(double newZ)
+    {
+        z = (float)newZ;
+    }
+
+}
+
+
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,15 +6,82 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public Animator animator;
     [SerializeField]
     private float _maxSpeed = 3.0f;
     private Vector2 _input;
 
+    bool facingLeft = false;
+    bool facingUp = false;
+    bool isMoving = false;
+   
+
+
     public void Move(InputAction.CallbackContext context) =>
         _input =  context.ReadValue<Vector2>();
     private void Update() {
+        float maxx;
         var velocity = new Vector3(_input.x, _input.y, 0.0f) * _maxSpeed;
-        //transform.position = new Vector3()(_input.x, _input.y,  transform.position.z)
+        if (Mathf.Abs(_input.x) > Mathf.Abs(_input.y))
+            maxx = Mathf.Abs(_input.x);
+        else
+            maxx = Mathf.Abs(_input.y);
+        animator.SetFloat("Speed", maxx);
+
+/*        pentru muzica
+ *        if (maxx > 0)
+            isMoving = true;
+        else
+            isMoving = false;
+        if (isMoving)
+        {
+            if (!audioSrc.isPlaying)
+                audioSrc.Play();
+        }
+        else
+            audioSrc.Stop();*/
+        
         transform.position += velocity * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (_input.y > 0)
+        {
+            animator.SetInteger("Direction", 2);
+            if (!facingUp)
+            { facingUp = !facingUp; }
+            
+        }
+        if (_input.y < 0 )
+        {
+            animator.SetInteger("Direction", 0);
+            if (facingUp)
+            { facingUp = !facingUp; }
+            
+        }
+        if (_input.x != 0 )
+        {
+            animator.SetInteger("Direction", 1);
+        }
+        if (_input.x > 0 && facingLeft)
+        {
+            Flip();
+        }
+        if (_input.x < 0 && !facingLeft)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingLeft = !facingLeft;
     }
 }
